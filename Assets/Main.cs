3c79deb91d728430a2c8d468e82676e2fccb2df8
ronald8192 <u23805@gmail.com>
2016-06-05 18:26:06 +0800

    upload game result script

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -16,6 +16,12 @@ public class Main : MonoBehaviour {
 		spawnObject = true;
 		scene = 1;
 
+		//test
+//		Debug.Log("a" == "a"); //true
+//		Debug.Log("a" == "b");//false
+//		Debug.Log(string.Compare("a","a")); //0
+//		Debug.Log(string.Compare("a","b")); //-1
+//		Debug.Log(string.Compare("b","a")); //1
 
 //		SimpleJSON example
 //		var json = "{\"version\": \"1.0\",\"data\":{\"sampleArray\":[\"string value\",5,{\"name\": \"sub object\"}]}}";
@@ -33,5 +39,35 @@ public class Main : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+
+	public void OnGameFinish(){
+		//create a new AsyncGameResultUploader
+		AsyncGameResultUploader resultUploader = gameObject.AddComponent<AsyncGameResultUploader> ();
+
+		//set callback, return WWW as object in `wwwObj`
+		resultUploader.SetCallback ((wwwObj) => {
+			WWW download = (WWW)wwwObj;
+
+			//check error by http status code
+			if(string.IsNullOrEmpty(download.error)) {
+				//HTTP 2xx
+				var jObj = JSON.Parse(download.text);
+				if(jObj["status"] == "OK"){
+					//success
+
+
+				}else{
+					//failed, usually missing some params
+
+
+				}
+
+			}else{
+				//HTTP 4xx / 5xx / ...
+				Debug.Log( "Upload game result error: " + download.error );
+			}
+		}).Start();
 	}
 }
---
+++ Assets/Scenes/menu/scripts/AsyncGameResultUploader.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsyncGameResultUploader : MonoBehaviour {
+	string url = "http://domain.com/api/game_result/new?";
+	string queryParams = "";
+	System.Action<object> callback;
+
+	public AsyncGameResultUploader SetCallback(System.Action<object> callback){
+		this.callback = callback;
+		return this;
+	}
+
+	// Use this for initialization
+	public IEnumerator Start () {
+		AddQueryParams("student_id", Main.currentStudent.GetId() + "");
+		AddQueryParams("duration", 300 + "");      //change to your value
+		AddQueryParams("object_total", 999 + "");  //change to your value
+		AddQueryParams("object_caught", 99 + "");  //change to your value
+
+		Debug.Log("Upload game result: " + url + queryParams);
+		WWW download = new WWW( url + queryParams );
+		yield return download;
+		callback(download);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/**
+	 * GET
+	 * construct query string (helper)
+	 **/
+	public string AddQueryParams(string key, string value){
+		queryParams += WWW.EscapeURL(key) + "=" + WWW.EscapeURL(value) + "&";
+		return queryParams;
+	}
+}
